0053ab4a96e0f30e084b4495011c7a9a9b2673d1
Prafful Daga <prraful@gmail.com>
2020-04-17 12:28:10 +0530

    day 1 - bootstrap and homepage!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shop/header/header.component';
+import { SlideshowComponent } from './shop/slideshow/slideshow.component';
+import { RegistrationComponent } from './shop/registration/registration.component';
+import { ShoutoutComponent } from './shop/shoutout/shoutout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlideshowComponent,
+    RegistrationComponent,
+    ShoutoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shop/shoutout/shoutout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoutout',
+  templateUrl: './shoutout.component.html',
+  styleUrls: ['./shoutout.component.css']
+})
+export class ShoutoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
